bbf7e51bfc3bfadeb61c4bb1ac4f45a61ef050af
kadir1278 <kadirari@MAU-BIM5>
2021-04-14 18:17:39 +0300

    View Eklendi

--- CRNProject_DataAccessLayer/obj/Debug/net5.0/CRNProject_DataAccessLayer.AssemblyInfo.cs
+++ CRNProject_DataAccessLayer/obj/Debug/net5.0/CRNProject_DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRNProject_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a19b06cd7a7d5ef2799e56ba3821e598411f11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f385bcd7be851eb5340218039ca73aaafc8dbce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRNProject_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRNProject_DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
