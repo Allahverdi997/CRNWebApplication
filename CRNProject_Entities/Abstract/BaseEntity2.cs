f385bcd7be851eb5340218039ca73aaafc8dbce7
kadir1278 <kadirari@MAU-BIM5>
2021-04-14 15:18:35 +0300

    Entities default değerler güncellendi

--- CRNProject_Entities/Abstract/BaseEntity2.cs
+++ CRNProject_Entities/Abstract/BaseEntity2.cs
@@ -1,6 +1,9 @@
 ﻿using CRNProject_CoreLayer.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +12,10 @@ namespace CRNProject_Entities.Abstract
 {
     public class BaseEntity2 : IEntity
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public DateTime LastDateTime { get; set; }
+        [DefaultValue("true")]
         public bool IsActive { get; set; }
     }
 }
